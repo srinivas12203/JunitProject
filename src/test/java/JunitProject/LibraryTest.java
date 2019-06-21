5c314e2cb420b44c5dfe859115c4752de253447e
C-PN19 <C-PN19@DAL-8CO094-9W7.wks.tsh.thomson.com>
2019-06-21 16:01:21 +0530

    Second commit

--- src/test/java/JunitProject/LibraryTest.java
+++ src/test/java/JunitProject/LibraryTest.java
@@ -10,6 +10,6 @@ public class LibraryTest {
     @Test 
     public void testSomeLibraryMethod() {
         Library classUnderTest = new Library();
-       assertEquals(121,12);
+       assertEquals(121,121);
     }
 }
